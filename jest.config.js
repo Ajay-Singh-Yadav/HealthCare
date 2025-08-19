be777f8531ace13b5bb5c03733599d43146e8155
Ajay Singh Yadav <ajayyadav442408@gmail.com>
2025-08-19 09:51:43 +0530

    Stylish App

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: ['./jest.setup.js'],
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native' +
+      '|@react-native' +
+      '|@react-navigation' +
+      '|react-native-vector-icons' +
+      '|react-native-gesture-handler' +
+      ')/)',
+  ],
 };
